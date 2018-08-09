666b033928d9f5359c4077db98db0014cd888b8a
Paul <paul.guerra@gmail.com>
2018-08-09 12:57:47 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Aplicación de comandos
+
+Este es un ejercicio del curso de Udemy de Node impartido por Fernando
+
+Recuerden instalar los paquetes de node
+
+
+```
+npm install
+```
---
+++ config/yargs.js
@@ -0,0 +1,23 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer.'
+};
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completada o pendiente la tarea'
+};
+
+
+
+const argv = require('yargs').command('crear', 'Crea un elemento por hacer', { descripcion })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', { descripcion, completado })
+    .command('listar', 'Muestra la lista de tareas por hacer')
+    .command('borrar', 'Borra una tarea', { descripcion })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
